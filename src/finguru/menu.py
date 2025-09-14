4b30e17a4f6fe5a117fd29b98a329f4f0cb3b270
rstraiker <rrs8@cin.ufpe.br>
2025-09-14 11:37:27 -0300

    func title and showOptions inplemented

--- src/finguru/menu.py
+++ src/finguru/menu.py
@@ -8,15 +8,16 @@ def showOptions(lista):
     for i, cat in enumerate(lista,1):
          print(f'   {i}. {cat}')
     print('='*50)
-    
+
+def title(frase):
+    print('='*50)
+    print('||'+ frase.center(46)+'||')
+    print('='*50)
 
 
 def menuDebitar():
     limpar_terminal()
-    print('='*50)
-    print("""||     Selecione Categoria da dispesa ||""")
-    print('='*50)
-    
+    title('Selecione Categoria da dispesa')
     categories = ['Comida', 'Contas', 'Lazer', 'Viagens'] #aqui vai puxar essas categoria do banco de dados
     showOptions(categories)
     opcao = int(input('Select: '))
@@ -26,29 +27,25 @@ def menuDebitar():
 
 def menuCreditar():
     limpar_terminal()
-    print('='*50)
-    print("""||            Selecione sua opção               ||""")
-    print('='*50)
-    
+    title('Selecione Opção de Credito')
     categoriesDeb = ['Comida', 'Contas', 'Lazer', 'Viagens'] #assim como na parte de debito aqui vai ser puxado de um banco de dados
                                                             # para adicionar a opção de reembolsos
     categories = ['Salario', 'Vendas', 'Bonus', 'Reembolso' ]
     showOptions(categories)
     opcao = int(input('Select: '))
     if categories[opcao-1] == 'Reembolso':
+        limpar_terminal()
+        title('Selecione Opção de Reembolso')
         showOptions(categoriesDeb)
         
         
     
 def menu():
+    
+    opcoes = ['Creditar', 'Debitar', 'Sair']
     limpar_terminal()
-    print('='*50)
-    print("""||            Selecione sua opção               ||""")
-    print('='*50)
-    print("   1. Creditar")
-    print("   2. Debitar")
-    print("   3. Sair")
-    print('='*50)
+    title('Selecione Sua Opção')
+    showOptions(opcoes)
     opcao = int(input('Select: '))
     
     match opcao:
