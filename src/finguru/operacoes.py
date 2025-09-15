4b4aa8b466691d31748e9d9f7e9a30eefac61daa
rstraiker <rrs8@cin.ufpe.br>
2025-09-15 09:30:23 -0300

    sqlmodel added

--- src/finguru/operacoes.py
+++ src/finguru/operacoes.py
@@ -1,7 +1,15 @@
+import sqlmodel as sm
+from typing import Optional
 
-def debitar(catgorie:str , valor:float ):
-    #fazer todo o processo
+class operacao(sm.SQLModel, table=True):
+    id: Optional[int] = sm.Field(default=None, primary_key=True)
+    categoria: str
+    valor: float
+    tipo: str  # 'debito' ou 'credito'
+
+def debitar(categoria: str, valor: float):
+    # fazer todo o processo
     print("operação realizada com sucesso")
     
-def creditar(catgorie:str , valor:float ):
-        print("operação realizada com sucesso")
+def creditar(categoria: str, valor: float):
+    print("operação realizada com sucesso")
